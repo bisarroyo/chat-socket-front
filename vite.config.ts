3106de4f1bbff81d4d636dfdb7adab8d481c9d47
Bismark Arroyo <bis.arroyo@gmail.com>
2023-10-18 19:44:16 -0600

    config alias and css modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // Define tus alias como pares clave-valor dentro de un objeto
+      '@components': path.join(__dirname, './src/components'),
+      '@pages': path.join(__dirname, './src/pages')
+    }
+  },
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[name]___[hash:base64:5]'
+    }
+  }
 })

33d0a0a13212e5f7427c48f11a0a4d081f56dcf6
Bismark Arroyo <bis.arroyo@gmail.com>
2023-10-19 20:19:02 -0600

    link styles

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,10 +1,12 @@
+import styles from '../styles/components/messages.module.css'
+
 interface MessagesProps {
   messages: string[]
 }
 
 const Messages: React.FC<MessagesProps> = ({ messages }) => {
   return (
-    <ul>
+    <ul className={styles.messagesContainer}>
       {messages.map((message, index) => (
         <li key={index}>{message}</li>
       ))}

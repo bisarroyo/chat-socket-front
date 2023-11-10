3297af9fe9a19a0786d1103184017af746d8633d
Bismark Arroyo <bis.arroyo@gmail.com>
2023-11-09 19:12:43 -0600

    add sidebar to the main page

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,9 +1,12 @@
 import ChatElement from '../components/ChatElement'
+import { Sidebar } from '../components/Sidebar'
+
 import styles from './chat.module.css'
 
 export default function Chat() {
   return (
     <div className={styles.mainContainer}>
+      <Sidebar />
       <ChatElement />
     </div>
   )

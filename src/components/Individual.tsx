563317fa1ed7f0bea5b5ff7fd4f920163aeba56f
Bismark Arroyo <bis.arroyo@gmail.com>
2023-11-09 19:20:55 -0600

    add styles to sidebar chats

--- src/components/Individual.tsx
+++ src/components/Individual.tsx
@@ -1,3 +1,13 @@
-export const Individual = ({ message }) => {
-  return <div>{message}</div>
+import styles from '../styles/components/individual.module.css'
+
+interface IndividualProps {
+  message: string
+}
+
+export const Individual: React.FC<IndividualProps> = ({ message }) => {
+  return (
+    <div className={styles.chat}>
+      <p>{message}</p>
+    </div>
+  )
 }
